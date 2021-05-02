c5d36c57b7b445313f59446bd379baa5b9825962
sarthakaditya52 <sarthak.aditya52@gmail.com>
2021-05-03 00:53:23 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from 'react'
 import NavBar from "./components/navbar/NavBar"
+import { Container, Row, Col } from "react-bootstrap";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Container className="base">
+        <Row>
+          <Col className="base-small" xs={1}>1 of 3</Col>
+          <Col>
+            <Home />
+          </Col>
+          <Col className="base-small" xs={1}>3 of 3</Col>
+        </Row>
+      </Container>
     </div>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home-container">
+            <h5>Hello, my name is</h5>
+            <h1>Sarthak Aditya.</h1>
+            <h2>I'm a fullstack developer.</h2>
+            <div className="home-content">
+                I am a graduate from <span><a href="https://www.iiitd.ac.in">IIIT-D</a></span> who loves building web and mobile applications. 
+                Currently, I'm working as a freelancer and looking for an opportunity to work for a company where I can work on 
+                exciting and challenging projects.
+            </div>
+        </div>
+    )
+}
+
+export default Home
